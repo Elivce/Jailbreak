d3989b7b746374a1c502f35ce46792589a4487fa
Elivce <84044265+Elivce@users.noreply.github.com>
2025-07-19 19:47:42 -0500

    Update camaroTeleporta.lua

--- camaroTeleporta.lua
+++ camaroTeleporta.lua
@@ -314,10 +314,8 @@ local function teleport(cframe, tried) -- unoptimized
 
     if vehicle_object then 
         local vehicle_distance = (vehicle_object.Seat.Position - player.Character.HumanoidRootPart.Position).Magnitude;
-        if isInVehicle() then
-            print('yes')
-            movement:move_to_position(vehicle_object.Engine, cframe, dependencies.variables.vehicle_speed, true);
-            print("Moving vehicle to:", cframe, "with speed:", dependencies.variables.vehicle_speed)
+        if 1+1 == 3 then
+            print("no")
         else 
             if vehicle_object.Seat.PlayerName.Value ~= player.Name then
                 movement:move_to_position(player.Character.HumanoidRootPart, vehicle_object.Seat.CFrame, dependencies.variables.player_speed, false, vehicle_object, tried);
@@ -344,11 +342,6 @@ local function teleport(cframe, tried) -- unoptimized
             end;
 
             movement:move_to_position(vehicle_object.Engine, cframe, dependencies.variables.vehicle_speed, true);
-
-            repeat -- attempt to exit car
-                task.wait(0.15);
-                dependencies.modules.character_util.OnJump();
-            until vehicle_object.Seat.PlayerName.Value ~= player.Name;
         end;
     else
         movement:move_to_position(player.Character.HumanoidRootPart, cframe, dependencies.variables.player_speed);
